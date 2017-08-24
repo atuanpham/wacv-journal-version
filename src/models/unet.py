b7be27d136491eace98200244d62d70cd38acac0
Pham Anh Tuan <tuan.phamanh@outlook.com>
2017-08-24 21:47:54 +0700

    come back previous code

--- src/models/unet.py
+++ src/models/unet.py
@@ -63,7 +63,7 @@ class Unet(object):
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
         conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-        conv10 = Conv2D(3, 1, activation='sigmoid')(conv9)
+        conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
 
         model = Model(inputs=inputs, outputs=conv10)
 
